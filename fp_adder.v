f3a895ff16834dccf6533f276eb6fd8274c97dce
Grisha Khachatryan <grisha1999.15@gmail.com>
2019-09-08 22:25:35 +0400

    Changed adder algorithm (unfinished)

--- fp_adder.v
+++ fp_adder.v
@@ -26,10 +26,8 @@ output reg [31:0] out;
 
 reg sign[2:0];
 reg [7:0] exp[2:0];
-reg [22:0] mantis[2:0];
+reg [27:0] mantis[2:0];
 reg carry;
-
-reg m[2:0];
 
 reg norm_sign;
 reg [7:0] norm_exp;
@@ -41,31 +39,34 @@ normalize_fp nm ({norm_sign, norm_exp, norm_mantis}, out_norm);
 
 always @(*)
 begin
-    {sign[0], exp[0], mantis[0]} = number_1;
-    {sign[1], exp[1], mantis[1]} = number_2;
-
-    //m[0] = mantis[0][0];
-    //m[1] = mantis[1][0];
+    {sign[0], exp[0], mantis[0]} = {number_1, 5'b00000};
+    {sign[1], exp[1], mantis[1]} = {number_2, 5'b00000};
+    mantis[0] = {1'b1, mantis[0][27:1]};
+    mantis[1] = {1'b1, mantis[1][27:1]};
 
     //$display("mantis[0] = %b; m[0] = %b", mantis[0], m[0]);
     //$display("mantis[1] = %b; m[1] = %b", mantis[1], m[1]);
 	
-	if (exp[0] != {8{1'b0}}) begin
+	/*if (exp[0] != {8{1'b0}}) begin
 	    {mantis[0], m[0]} = {1'b1, mantis[0]};
-    	    exp[0] = exp[0] + 1;
+    	exp[0] = exp[0] + 1;
 	end else $finish;
 
 	if (exp[1] != {8{1'b0}}) begin
 	    {mantis[1], m[1]} = {1'b1, mantis[1]};
-    	    exp[1] = exp[1] + 1;
-	end else $finish;
+    	exp[1] = exp[1] + 1;
+	end else $finish;*/
 
     if (exp[0] > exp[1]) begin
-        {mantis[1], m[1]} = {mantis[1], m[1]} >> (exp[0] - exp[1]);
+        //mantis[1] = {1'b1, mantis[1][25:0]};
+        //exp[1] = exp[1] + 1;
+        mantis[1] = mantis[1] >> (exp[0] - exp[1]);
         exp[2] = exp[0];
     end
     else if (exp[0] < exp[1]) begin
-        {mantis[0], m[0]} = {mantis[0], m[0]} >> (exp[1] - exp[0]);
+        //mantis[0] = {1'b1, mantis[0][25:0]};
+        //exp[0] = exp[0] + 1;
+        mantis[0] = mantis[0] >> (exp[1] - exp[0]);
         exp[2] = exp[1];
     end
     else begin
@@ -73,19 +74,70 @@ begin
     end
 
     if (~(sign[0]^sign[1])) begin
-        {carry, mantis[2], m[2]} = {mantis[0], m[0]} + {mantis[1], m[1]};
+        {carry, mantis[2]} = mantis[0] + mantis[1];
         sign[2] = sign[0];
-	//mantis[2] = mantis[2] + (m[0] ^ m[1]);
-        
-	if (!carry) begin
-            mantis[2] = {mantis[2][21:0], m[2]};
-            exp[2] = exp[2] - 1;
+        //if (exp[0] == exp[1]) begin
+        //    mantis[2] = {carry, mantis[2][25:0]};
+        //    exp[2] = exp[2] + 1;
+//
+        //    $display("mantis[2] = %b", mantis[2]);
+        //    /*if (mantis[2][2:0] >= 3'b100) begin
+        //        if (mantis[2][2:0] == 3'b100) begin
+        //            mantis[2][25:3] = mantis[2][25:3] + mantis[2][3];
+        //        end
+        //        else begin
+        //            mantis[2][25:3] = mantis[2][25:3] + 1'b1;
+        //            $display("mtav stex");
+        //        end
+        //    end*/
+        //end
+        //else begin
+            $display ("mantis[2] = %b", mantis[2]);
+            if (mantis[2][3:0] >= 4'b1000)
+            begin
+                mantis[2][26:4] = mantis[2][26:4] + 1'b1;
+            end
+            /*else if (mantis[2][3:0] == 4'b1000) begin
+             mantis[2][26:4] = mantis[2][26:4] + mantis[2][4];
+            end*/
+            if (carry) begin
+                mantis[2] = mantis[2] >> 1;
+                mantis[2][27] = carry;
+                //mantis[2][26:2] = {carry, mantis[2][26:3]};
+                exp[2] = exp[2] + 1;
+            end
+            /*if (mantis[2][2:0] >= 3'b100) begin
+                if (mantis[2][2:0] == 3'b100) begin
+                    mantis[2][25:3] = mantis[2][25:3] +mantis[2][3];
+                    $display("mantis[2][3] = %b", mantis[2][3]);
+                end
+                else if (mantis[2][2:0] > 3'b100) begin
+                    mantis[2][25:3] = mantis[2][25:3] + 1'b1;
+                    $display("mtav stex");
+                end
+            end*/
+        //end
+        $display ("mantis[2][4:0] = %b", mantis[2][4:0]);
+        if (mantis[2][3:0] > 4'b1000)
+        begin
+            mantis[2][26:4] = mantis[2][26:4] + 1'b1;
         end
+        else if (mantis[2][3:0] == 4'b1000) begin
+             mantis[2][26:4] = mantis[2][26:4] + mantis[2][4];
+        end
+
+        /*casex (mantis[2][2:0])
+            3'b0xx: mantis[2][25:3] = mantis[2][25:3] + 1'b0;
+            3'b100: mantis[2][25:3] = mantis[2][25:3] + mantis[2][3];
+            default: mantis[2][25:3] = mantis[2][25:3] + 1'b1;
+        endcase*/
+
+        if (exp[2] == 8'hff) mantis[2] = 0;
 
 	//mantis[2] = mantis[2] + (m[0] + m[1]);
     end
     else if (sign[0]|sign[1]) begin
-        if ({mantis[0], m[0]} >= {mantis[1], m[1]})
+        /*if ({mantis[0], m[0]} >= {mantis[1], m[1]})
         begin
             {mantis[2], m[2]} = {mantis[0], m[0]} - {mantis[1], m[1]};
             sign[2] = sign[0];
@@ -93,21 +145,33 @@ begin
         else begin
             {mantis[2], m[2]} = {mantis[1], m[1]} - {mantis[0], m[0]};
             sign[2] = sign[1];
+        end	    
+	    if (mantis[2][22] != 1'b1) begin
+	    	{mantis[2], m[2]} = {mantis[2][21:0], m[2], 1'b0};
+	    	exp[2] = exp[2] - 1;
+            $display("mtav"); 
+	    end*/
+
+        if (mantis[0] >= mantis[1])
+        begin
+            mantis[2] = mantis[0] - mantis[1];
+            sign[2] = sign[0];
         end
-	$display("sign = %b", sign[2]);
-	if (mantis[2][22] != 1'b1) begin
-		{mantis[2], m[2]} = {mantis[2][21:0], m[2], 1'b0};
-		exp[2] = exp[2] - 1;
-	end
-	norm_exp = exp[2];
-	norm_mantis = mantis[2];
-	norm_sign = sign[2];
+        else begin
+            mantis[2] = mantis[1] - mantis[0];
+            sign[2] = sign[1];
+        end	    
+
+	    norm_exp = exp[2];
+	    norm_mantis = mantis[2];
+	    norm_sign = sign[2];
         #2;
-	exp[2] = out_norm[30:23]-1;
-	sign[2] = out_norm[31];
-	mantis[2] = {out_norm[21:0], m[2]};
+	    exp[2] = out_norm[30:23]-1;
+	    sign[2] = out_norm[31];
+	    //mantis[2] = {out_norm[21:0], m[2]};
+        mantis[2] = out_norm[22:0] << 1;
     end 
-    out = {sign[2], exp[2], mantis[2]};
+    out = {sign[2], exp[2], mantis[2][26:4]};
 end
 
 endmodule
--- fp_adder_tb.v
+++ fp_adder_tb.v
@@ -36,59 +36,51 @@ fp_adder _inst (.number_1(number_1), .number_2(number_2), .out(out));
 end*/
 
 /*initial begin
-    number_1 = 0;
-    number_2 = 0;
-#10 number_1 = 32'b01000000001000000000000000000000;
-    number_2 = 32'b01000000011000000000000000000000;
-#10 number_1 = 32'b01000000000100110011001100110011;
-    number_2 = 32'b01000000011000000000000000000000;
-#10 number_1 = 32'b01000000101000000000000000000000;
-    number_2 = 32'b01000000001000000000000000000000;
-#10 number_1 = 32'b01000000011000000000000000000000; // 0x40600000
-    number_2 = 32'b11000000000000000000000000000000; // 0xC0000000
-    //sum = b00111111110000000000000000000000, 0x3FC00000
+	number_1 = 32'b01000000001111110000000000011111;
+	number_2 = 32'b01000001011111110000110000000001;
+	#5;
+	$display("out_test = %h, %b", out, out);
+	$display("out_real = %h, %b", $shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)), $shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)));
 
 #20 $finish;
 end*/
 
 initial begin
-    repeat (100) begin
-		sign[0] = $random;
-		sign[1] = $random;
-		exp[0] = $random%255 + 1;
-		exp[1] = $random%255 + 1;
-		mantis[0] = $random;
-		mantis[1] = $random;
-		number_1 = {sign[0], exp[0], mantis[0]};
-		number_2 = {sign[1], exp[0], mantis[1]};
+    repeat (10000) begin
+		sign[0] = $urandom;
+		sign[1] = $urandom;
+		exp[0] = $urandom%254 + 1;
+		exp[1] = $urandom%254 + 1;
+		mantis[0] = $urandom;
+		mantis[1] = $urandom;
+
+		mantis[0][0] = 1'b1;
+		mantis[1][0] = 1'b0;
+		number_1 = {0&sign[0], exp[0], mantis[0]};
+		number_2 = {0&sign[0], exp[1], mantis[1]};
 		#5;
-	
-		/*if ($shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)) != out)
+		$display("N_1 = %h, %b", number_1, number_1);
+		$display("N_2 = %h, %b", number_2, number_2);
+		$display("mantis[0] = %b", _inst.mantis[0]);
+		$display("mantis[1] = %b", _inst.mantis[1]);
+		$display("mantis[2] = %b", _inst.mantis[2]);
+		$display("carry = %b", _inst.carry);
+		$display("out_test = %h, %b", out, out);
+		$display("out_real = %h, %b", $shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)), $shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)));
+		$display("exp[1]-exp[0] = %d", _inst.exp[1] - _inst.exp[0]);
+		$display("exp[0]-exp[1] = %d", _inst.exp[0] - _inst.exp[1]);
+		$display("Real %f + %f = %f", $bitstoshortreal(number_1), $bitstoshortreal(number_2), $bitstoshortreal(number_1) + $bitstoshortreal(number_2));
+		$display("Test %f + %f = %f", $bitstoshortreal(number_1), $bitstoshortreal(number_2), $bitstoshortreal(out));
+		if ($shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)) != out)
 		begin
 			$display("Fail");
-			$display("mantis_2 = %b", _inst.mantis[2]);
-			$display("N_1 = %h, %b", number_1, number_1);
-			$display("N_2 = %h, %b", number_2, number_2);
-			$display("carry = %b", _inst.carry);
-			$display("out = %h, %b", out, out);
-			$display("out_test = %h, %b", $shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)), $shortrealtobits($bitstoshortreal(number_1) + $bitstoshortreal(number_2)));
-			$display("exp[1]-exp[0] = %d", _inst.exp[1] - _inst.exp[0]);
-			$display("exp[0]-exp[1] = %d", _inst.exp[0] - _inst.exp[1]);
-			$display("************");
+			$display("*************************************");
 			$stop;
 		end
 		else begin
-			//$display("Pass");
-		end*/
-		$display("mantis_2 = %b", _inst.mantis[2]);
-		$display("N_1 = %h, %b", number_1, number_1);
-		$display("N_2 = %h, %b", number_2, number_2);
-		$display("carry = %b", _inst.carry);
-		$display("out = %h, %b", out, out);
-		$display("exp[1]-exp[0] = %d", _inst.exp[1] - _inst.exp[0]);
-		$display("exp[0]-exp[1] = %d", _inst.exp[0] - _inst.exp[1]);
-		$display("************");
-		
+			$display("Pass");
+			$display("*************************************");
+		end
     end
 
     #20 $finish;
