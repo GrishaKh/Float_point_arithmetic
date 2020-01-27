540001dc799d592939d09d1cbeba4d53e9d8e305
Grisha <grisha1999.15@gmail.com>
2020-01-27 15:54:50 +0400

    Fix bug in adder

--- src/adder.v
+++ src/adder.v
@@ -1,37 +1,56 @@
-module adder (
-    sign_A, sign_B,
-    exp, sign,
-    mantis_A, mantis_B,
-    exp_out, mantis_out,
-    loss, operator
+module adder
+(
+    sign_A,
+    sign_B,
+    exp,
+    mantis_A,
+    mantis_B,
+    sign,
+    exp_out,
+    mantis_out,
+    loss,
+    operator
 );
 
-input sign_A, sign_B;
-input [7:0] exp;
-input [25:0] mantis_A, mantis_B;
-output sign;
-output [7:0] exp_out;
+input        sign_A;
+input        sign_B;
+input [7:0]  exp;
+input [25:0] mantis_A;
+input [25:0] mantis_B;
+
+output        sign;
+output [7:0]  exp_out;
 output [25:0] mantis_out;
-output loss;
-output operator;
+output        loss;
+output        operator;
 
-wire carry;
-wire [25:0] mantis_sum, mantis_shifted;
+wire        carry;
+wire [25:0] mantis_sum;
+wire [25:0] mantis_shifted;
+wire [7:0]  exp_tmp;
 
-assign operator = sign_A^sign_B;
-assign {carry, mantis_sum} = operator ? mantis_A - mantis_B : mantis_A + mantis_B;
-assign sign = sign_A;
+assign operator            = sign_A^sign_B;
+assign {carry, mantis_sum} = operator ? mantis_A - mantis_B:
+                                        mantis_A + mantis_B;
+assign sign    = sign_A;
+assign exp_out = operator & ~|mantis_sum ? 8'h00 : exp_tmp;
 
-shifter #(.DIRECTION(1)) __shifter (
+shifter
+#(
+    .DIRECTION(1)
+)
+__shifter
+(
     .exp (exp),
     .exp_target_or_diff ({{7{1'b0}}, carry}),
     .mantis(mantis_sum),
-    .exp_out (exp_out),
+    .exp_out (exp_tmp),
     .mantis_out (mantis_shifted),
     .loss (loss)
 );
 
-assign mantis_out = carry ? {carry, mantis_shifted[24:0]} : mantis_shifted;
+assign mantis_out = carry ? {carry, mantis_shifted[24:0]}:
+                                           mantis_shifted;
 
 
-endmodule
+endmodule // adder
